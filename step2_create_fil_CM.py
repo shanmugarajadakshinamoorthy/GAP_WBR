4023bb8dd7b4002fa2b5b371716cdc3af6c72488
shanmugarajadakshinamoorthy <shanmugaraja.dak@tigeranalytics.com>
2025-03-14 23:44:53 +0530

    Update step2_create_fil_CM.py

--- step2_create_fil_CM.py
+++ step2_create_fil_CM.py
@@ -7,7 +7,7 @@ def process_data(filtered_df, kb_file, output_file):
     with open(kb_file, "r") as f:
         kb_data = json.load(f)
 
-    nodes_kb = {node["id"]: (node["low"], node["high"]) for node in kb_data["nodes"]}
+    nodes_kb = {node["id"]: (node["low_pass"], node["high_pass"]) for node in kb_data["nodes"]}
     edges_kb = {(edge["start"], edge["end"]) for edge in kb_data["edges"]}
 
     # Identify significant variables
@@ -19,8 +19,16 @@ def process_data(filtered_df, kb_file, output_file):
         
         node_key = key.replace("_YoY", "")
         low, high = nodes_kb[node_key]
-        if low <= value <= high:
+        if node_key == 'demand':
             significant_nodes[node_key] = value
+        elif value< low:
+            significant_nodes[node_key] = value
+        elif value> high:
+            significant_nodes[node_key] = value
+            # print("key:", key)
+            # print("value:", value)
+    
+    # print("we are here:", significant_nodes)
 
     # Identify significant edges
     significant_edges = []
@@ -53,33 +61,4 @@ def process_data(filtered_df, kb_file, output_file):
 # output = process_data(filtered_df, "causal_maps/KB_Brand A.json", "derived/fil_data_causal_map.json")
 # print(json.dumps(output, indent=2))
 
-derived_folder = "derived"
-causal_maps_folder = "causal_maps"
-new_cm_folder = "CM_filtered"
 
-os.makedirs(new_cm_folder, exist_ok=True)
-
-# Loop through all JSON files in the derived folder
-for filename in os.listdir(derived_folder):
-    if filename.endswith("_filtered_data.json"):
-        brand_name = filename.split("_filtered")[0]  # Extract brand name (Brand_A, Brand_B, etc.)
-
-        # Construct file paths
-        input_file = os.path.join(derived_folder, filename)
-        kb_file = os.path.join(causal_maps_folder, f"KB_{brand_name.replace("Brand_", "Brand ")}.json")
-        output_file = os.path.join(new_cm_folder, f"{brand_name}_causal_map.json")
-
-        print(input_file)
-        print(kb_file)
-        print(output_file)
-    
-        # Load JSON data
-        with open(input_file, "r") as file:
-            filtered_df = json.load(file)
-
-        # Call process_data function
-        output = process_data(filtered_df, kb_file, output_file)
-
-        # Print output
-        # print(f"Processed {filename} -> {output_file}")
-        # print(json.dumps(output, indent=2))
