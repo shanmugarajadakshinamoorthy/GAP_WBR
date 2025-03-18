7fb596e0a9d8f8fa08a9a86e36b130671c607a88
Mkhatri <mayank_khatri@gap.com>
2025-03-18 14:53:24 +0530

    tabs moved to sidebar

--- ui/app.py
+++ ui/app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
 from backend import (
     update_summary,
     return_plot,
@@ -15,6 +17,15 @@ with st.sidebar:
     st.divider()
     st.subheader("WBR Summary")
     st.markdown("*Accelerate executive insights to action cycle*")
+    # org_level, brand_level = st.tabs(["WBR Summary", "Analyst WBR edits"])
+    selected_tab = option_menu(None, ["WBR Summary", "Analyst WBR edits"],icons=None, menu_icon="cast", default_index=0, orientation="vertical",
+                                   styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "0px"}, 
+        "nav-link": {"font-size": "15px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"font-size": "15px", "font-weight":"normal"},
+    })
+
 
 col1,col2 = st.columns(2)
 with col1:
@@ -22,9 +33,11 @@ with col1:
 
 with col2:
     st.selectbox("Date range", options=["Aug 2024", "Sep 2024", "Oct 2024", "Nov 2024", "Dec 2024", "Jan 2025"], disabled=False, label_visibility="hidden")
-org_level, brand_level = st.tabs(["WBR Summary", "Analyst WBR edits"])
 
-with org_level:
+# org_level, brand_level = st.tabs(["WBR Summary", "Analyst WBR edits"])
+
+# with org_level:
+if selected_tab == "WBR Summary":
     st.subheader("Summary")
     # st.markdown("**Org**")
     st.markdown("XYZ Solutions is a technology consulting firm specializing in AI-driven automation and digital transformation. We help businesses streamline operations, enhance efficiency, and drive growth with innovative solutions.")
@@ -37,8 +50,9 @@ with org_level:
 
 
 
-with brand_level:
-
+# with brand_level:
+if selected_tab == "Analyst WBR edits":
+    summaries = return_summary()
     table = return_table()
     plot = return_plot()
     selected_brand = st.selectbox("Select brand",options=summaries.keys())
